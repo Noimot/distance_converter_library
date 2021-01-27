c4012064ffa3114d3754a39198cce356160899d6
alabi noimot <kikkyal@gmail.com>
2021-01-27 13:34:46 +0100

    distance convert library

---
+++ distance_converter.js
@@ -0,0 +1,39 @@
+
+function Converter(convertFrom, convertTo) {
+    this.convertFrom = convertFrom;
+    this.convertTo = convertTo;
+
+}
+
+Converter.prototype.convertKmToM = function (number) {
+    return number * 1000;
+}
+
+Converter.prototype.convertMToKm = function (number) {
+    return number / 1000;
+}
+
+Converter.prototype.convertMilesToKm = function (number) {
+    return number * 1.609344;
+}
+
+
+Converter.prototype.convertKmToMiles = function (number) {
+    return number / 1.609344;
+}
+
+
+Converter.prototype.convertCmToM = function (number) {
+    return number * 100;
+}
+
+
+Converter.prototype.convertMToCm = function (number) {
+    return number / 100;
+}
+
+
+Converter.prototype.clearButton = function (number) {
+    return number = "";
+}
+
